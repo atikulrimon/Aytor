d15110044000a91d8f4e844b4385479fd6474209
atikulrimon <aislamrimon19@gmail.com>
2023-07-22 12:17:20 +0600

    Shop Page Change

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,57 +1,15 @@
-$(window).on('load',function(){
-    $('#preloader').hide();
-  })
+let shopGridBtn = document.querySelector(".shopGridBtn")
+let shopListBtn = document.querySelector(".shopListBtn")
+let shopProductBoxGrid = document.querySelector(".shopProductBoxGrid")
+let shopProductBoxList = document.querySelector(".shopProductBoxList")
 
-$(function(){
-  // Common Js Start
-  $('button').on('click',function(){
-    return false
-  })
-  // Common Js End  
-  
-  //Banner Slick Start Here
-    $('.bannerSlider').slick({
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        fade: true,
-        dots: true,
-        dotsClass: 'bannerSliderDots'
-        
-      });
-    // Banner Slick End
+shopGridBtn.addEventListener("click", function(){
+  shopProductBoxGrid.style.display = "show(1000)";
+  shopProductBoxList.style.display - "hide()"
+})
 
-    // Newsletter Popup Start
-      setTimeout(function(){
-        document.querySelector('#newsletter').style.transform ='scale(1)'
-      },2000)
-
-      function newsPopUpClose(){
-        document.querySelector('#newsletter').style.transform = 'scale(0)'
-      }
-      
-      document.querySelector('.newsletterClose').addEventListener('click',newsPopUpClose)
-
-      $("#newsletter").click(function(){
-        
-      });
-    // Newsletter Popup End
-
-    // Back TO Top Start
-    $(window).on('scroll', function(){
-      let scrollTopSize = $(this).scrollTop();
-      
-      if(scrollTopSize > 500){
-        $('.backToTopBtn').show(500);
-      }else{
-        $('.backToTopBtn').hide(500);
-      }
-    })
-    $('.backToTopBtn').on('click', function(){
-        $('body,html').animate({
-          scrollTop: 0
-        },1000)
-    })
-  // Back TO Top End
-
+shopListBtn.addEventListener("click", function(){
+  shopProductBoxGrid.style.display = "hide()";
+  shopListBtn.classList.add(".shopProduct_active")
+  shopProductBoxList.style.display = "show(1000)"
 })
--- assets/js/shopDetails.js
+++ assets/js/shopDetails.js
@@ -16,5 +16,12 @@ $(function(){
     
  });
 
+ $('.reletedProductContent').slick({
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   arrows: false
+ });
 
 })
